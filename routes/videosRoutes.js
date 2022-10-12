1416150c805c8dc83169d70a486d0a2da449149a
Omar Mahmoud <99248837+OmarMahmoud97@users.noreply.github.com>
2022-10-12 10:32:15 +0100

    add res.json rather than res.get

--- routes/videosRoutes.js
+++ routes/videosRoutes.js
@@ -7,26 +7,19 @@ router
     function isMatch(video) {
       return video.id === req.params.id;
     }
-    console.log(req.params.id);
-    res.send(videoData.find(isMatch));
+    res.json(videoData.find(isMatch));
   })
   .get("/", (req, res) => {
-    res.send(videoData);
+    res.json(videoData);
   })
   .post("/", (req, res) => {
-    // create a new video
-    // receive details all of video from client
-    // receive object that has all the data that the videos have
-    console.log(videoData.length);
     videoData.push(req.body);
-    console.log(videoData.length);
-
     fs.writeFile("./data/videos.json", JSON.stringify(videoData), (err) => {
       if (err) {
         console.error(err);
       }
       console.log("success");
     });
-    res.send("receive post");
+    res.json("receive post");
   });
 module.exports = router;
